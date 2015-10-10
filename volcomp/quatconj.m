227d89fcdd7e55500d916fbc7f4d035252a19ce7
Charles Karney <charles.karney@sri.com>
2015-10-10 18:32:50 -0400

    Remove some work files.

--- volcomp/quatconj.m
+++
@@ -1,5 +0,0 @@
-% conjugate of a quaternion -- works on a set of quaternions
-% $Id$
-function y = quatconj(x)
-  y = [x(:,1), -x(:,2:4)];
-end
--- volcomp/quatnorm.m
+++
@@ -1,8 +0,0 @@
-% normalized quaternion -- works on a set of quaternions
-% $Id$
-function y = quatnorm(x)
-  y = x./repmat(sqrt(sum(x.^2, 2)), 1, 4);
-end
-
-
-
